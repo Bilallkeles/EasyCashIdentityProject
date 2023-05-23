8c0cc6656f2577f4ca1dd46ba2a2cf663f500be5
Bilallkeles <88910184+Bilallkeles@users.noreply.github.com>
2023-05-23 09:14:21 +0300

    paketler kuruldu

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("ad alanı boş geçmeyin");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("soyadı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("mail adresi boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("şifrenizi girin");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("şifrenizi girin");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("şifre uzunluğu");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Password");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        //[Required(ErrorMessage = "ad alanı zorunulu alan")]
+        //[Display(Name = "isim")]
+        //[MaxLength(100, ErrorMessage = "En fazla 100 karakter girilir.")]
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
